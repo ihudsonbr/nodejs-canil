7d2c35ac36e8264d2fb7b90182673730b117abc0
Hudson Vinicius <ihudson@live.com>
2023-08-12 13:57:29 -0300

    base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+    res.send('Home no controller');
+};
+
+export const cachorros = (req: Request, res: Response) => {
+    
+};
+
+export const gatos = (req: Request, res: Response) => {
+    
+};
+
+export const peixes = (req: Request, res: Response) => {
+    
+};
